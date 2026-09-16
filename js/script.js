ffb882829ddd92208284b40547da1a4b5095e84b
Sakina <sakinask3333@gmail.com>
2026-09-16 19:10:23 +0530

    Add initial JavaScript for Smart Tiffin

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Smart Tiffin JavaScript
+
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Smart Tiffin Loaded");
+});
+
+function loginSuccess() {
+    alert("Welcome Back! Login Successful.");
+}
+
+function signupSuccess() {
+    alert("Account Created Successfully!");
+}
